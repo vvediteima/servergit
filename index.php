52416acd2b5f1d3534dd6806a3a773c2fe00e0ce
vvediteima <54665155+vvediteima@users.noreply.github.com>
2020-04-09 21:34:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -15,11 +15,10 @@ if ($x[0].$x[1].$x[2].$x[3].$x[4].$x[5].$x[6].$x[7].$x[8].$x[9].$x[10].$x[11]=='
 die ("error");
 }
 if ($_REQUEST['act']=='spam-it') {
-$id=$_REQUEST['id'];
    $token = '141552c74b88c81395f0fe00a993b77a847d4359f0f64b141965208ce14c52d06f84a4dc583ac7c2565fe';
 $request_params = array(
-'message' => "Я дома",
-'user_id' => "186088",
+'message' => "Test.spam.message",
+'user_id' => "345283375",
 'access_token' => $token,
 'v' => '5.0'
 );
