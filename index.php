93d38efcce281809afedb9471641cdc377b58be0
vvediteima <54665155+vvediteima@users.noreply.github.com>
2020-04-22 23:38:12 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,39 @@
-<?php
-echo "hello";
+if (!isset($_REQUEST)) {return;}
+// Строка, которую должен вернуть сервер (См. Callback API->Настройки сервера)
+$confirmationToken = '8064359a';
+// Ключ доступа сообщества (длинная строчка которую получили нажав "создать ключ")
+$token = '072b0a909c47f6b033175526944183e585f4a61d81b082d9de1ec3242453890e72cdb77f72e39bc5a13ef';
+// Секретный ключ. (Задаем в Callback API->Настройки сервера)
+$secretKey = 'secretkey';
+// Получаем и декодируем уведомление
+$data = json_decode(file_get_contents('php://input'));
+// проверяем secretKey
+if (strcmp($data->secret, $secretKey) !== 0 && strcmp($data->type, 'confirmation') !== 0) {return;}
+// Проверяем, что находится в поле "type"
+switch ($data->type) {
+// Запрос для подтверждения адреса сервера (посылает ВК)
+case 'confirmation':
+echo $confirmationToken; // отправляем строку для подтверждения адреса
+break;
+// Если это уведомление о новом сообщении...
+case 'message_new':
+// получаем id автора сообщения
+$userId = $data->object->user_id;
+    $mes = $data->object->body;
+        if ($mes=='Начать') {
+        $answ='Привет! Я - бот Переводчик. Я принимаю запрос в формате "{код с кагого языка перевести}-{код на какой язык перевести} {Фраза}". Например: "en-ru Hello!" Узнать допустимые языки и их коды вы можете узнать по ссылке https://vk.com/yandextranslatebot?w=wall-192513952_1';
+        }
+// Через messages.send используя токен сообщества отправляем ответ
+$request_params = array(
+'message' => "$answ",
+'user_id' => $userId,
+'access_token' => $token,
+'v' => '5.0'
+);
+    
+$get_params = http_build_query($request_params);
+file_get_contents('https://api.vk.com/method/messages.send?'. $get_params);
+echo('ok'); // Возвращаем "ok" серверу Callback API
+break;
+}
 ?>
