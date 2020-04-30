b2635a9f2fc2b2c65af03000bb340fee0b1370b4
vvediteima <54665155+vvediteima@users.noreply.github.com>
2020-04-30 18:08:35 +0300

    Update syumshbot.php

--- syumshbot.php
+++ syumshbot.php
@@ -23,12 +23,12 @@ $userId = $data->object->user_id;
     $mes = $data->object->body;
              $uinfo=file_get_contents("https://api.vk.com/method/users.get?user_ids=$userId&access_token=659d3687f664f8590731be425ae93f42b2c865e7d67868b80049cf09068a6b221ec7d326dc503135fe80c&v=5.103");
 $uinfo=json_decode($uinfo,1);
-        $answ="Прости, ".$uinfo["response"][0]['first_name'].". Я не знаю такой команды. Напиши 'coms'"; 
+        $answ="Прости, ".$uinfo["response"][0]['first_name'].", я не знаю такой команды. Напиши coms"; 
  if ($mes=="Начать") {
 $answ="Привет, ".$uinfo["response"][0]['first_name']."!"; 
  }
  if ($mes=="coms" || $mes="Coms") {
-$answ="Вот список команд:\n Пока пусто" 
+$answ="Вот список команд:\n Пока пусто";
  }
         
 // Через messages.send используя токен сообщества отправляем ответ
